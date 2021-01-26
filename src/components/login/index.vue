52bfcf84fa03103a19acaae9156b77ad5422e7f2
scofield1906 <13058004410@163.com>
2021-01-26 20:56:55 +0800

    1.26_1

---
+++ src/components/login/index.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="login">
+        <p>
+            <input type="text" placeholder="请输入帐号">
+        </p>
+        <p>
+            <input type="text" placeholder="请输入密码">
+        </p>
+        <p>
+            <input type="submit" value="登陆">
+        </p>
+    </div>
+</template>
+
+<script>
+export default {
+    name:'login'
+}
+</script>
+
+<style>
+.login p{text-align: center;}
+.login p input{line-height:60px;width:100%;text-align: center;font-size:35px}
+</style>
